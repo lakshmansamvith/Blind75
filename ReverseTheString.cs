c6660a91a363351fdec849d334e6b53663796cc4
Lakshman Samvith .D <lakshmandunna75@gmail.com>
2023-06-25 12:36:31 +0530

    Create ReverseTheString.cs

---
+++ ReverseTheString.cs
@@ -0,0 +1,10 @@
+public class Solution
+{
+    public string ReverseWords(string sentence)
+    {
+
+        return string.Join(' ', sentence.Split(' ').Reverse().Where(word => word.Length > 0));
+//If you do not want to use LINQ, you can simply iterate through the same and get the same thing done. I simply chose a shortcut
+
+    }
+}
