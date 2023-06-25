bcf483569dfe77944a3a21df97d62400020459df
Lakshman Samvith .D <lakshmandunna75@gmail.com>
2023-06-25 12:10:58 +0530

    Create FindHighestAltitude.cs

---
+++ FindHighestAltitude.cs
@@ -0,0 +1,16 @@
+public class Solution
+{
+    public int LargestAltitude(int[] gain)
+    {
+        int maxAltitude = 0, sum=0; 
+        foreach(var altitude in gain)
+        {
+            sum = sum + altitude; 
+            if(sum > maxAltitude)
+            {
+                maxAltitude = sum; 
+            }
+        }
+        return maxAltitude; 
+    }
+}
